618ef782f03a0b91806d90fb8ab1319bbba3fe2c
장원정 <jang_expedition@gmail.com>
2025-03-10 14:45:42 +0900

    feat: DefaultLayout에 첫 페이지가 아닐 경우 뒤로 가는 버튼 나오도록 추가

--- src/components/DefaultLayout.tsx
+++ src/components/DefaultLayout.tsx
@@ -1,14 +1,36 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
 import { Card, CardContent, CardHeader, CardTitle } from "./ui";
 
 export function DefaultLayout() {
+  const location = useLocation();
+
   return (
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col">
         <div className="flex flex-col gap-6">
           <Card>
             <CardHeader className="text-center">
-              <CardTitle className="text-xl">My Picture Diary</CardTitle>
+              <CardTitle className="relative text-xl">
+                {location.pathname !== "/" && (
+                  <Link to={"/"} className="absolute top-1 left-0">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      className="lucide lucide-chevron-left"
+                    >
+                      <path d="m15 18-6-6 6-6" />
+                    </svg>
+                  </Link>
+                )}
+                My Picture Diary
+              </CardTitle>
             </CardHeader>
             <CardContent>
               <Outlet />
